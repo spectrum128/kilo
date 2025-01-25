1ce96282c953a90f4cefe4eb8b95370cd32e82b4
spectrum128 <declandoherty@hotmail.co.uk>
2025-01-25 15:56:06 +0000

    step 37: add abuf to manage dynamic string buffer

--- kilo.c
+++ kilo.c
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <string.h>
 
 /*** defines ***/
 
@@ -135,6 +136,30 @@ int getWindowSize(int *rows, int *cols){
         *rows = ws.ws_row;
         return 0;
     }
+}
+
+
+/*** append buffer ***/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+
+    if(new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
 }
 
 /*** output ***/
