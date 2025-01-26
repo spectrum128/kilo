f35483c511528a10d5d3027f19600fc78dc52941
spectrum128 <declandoherty@hotmail.co.uk>
2025-01-26 12:37:30 +0000

    step 45: move cursor with w,a,s,d.

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@
 /*** data ***/
 
 struct editorConfig{
+    int cx, cy;
     int screenrows;
     int screencols;
 
@@ -205,8 +206,11 @@ void editorRefreshScreen(){
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
 
-    abAppend(&ab, "\x1b[H", 3);
     abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -216,6 +220,23 @@ void editorRefreshScreen(){
 
 
 /*** input ***/
+
+void editorMoveCursor(char key){
+    switch(key){
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
 
 
 void editorProcessKeypress(){
@@ -227,6 +248,13 @@ void editorProcessKeypress(){
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            editorMoveCursor(c);
+            break;
     }
 }
 
@@ -234,6 +262,9 @@ void editorProcessKeypress(){
 /*** init ***/
 
 void initEditor(){
+    E.cx = 0;
+    E.cy = 0;
+
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
