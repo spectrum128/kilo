379dc977cf93ceba273351c155f52531ade761d3
spectrum128 <declandoherty@hotmail.co.uk>
2025-09-02 19:16:45 +0100

    step 104: handle backspace, del, enter etc.

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -505,6 +506,10 @@ void editorProcessKeypress(){
     int c = editorReadKey();
 
     switch(c){
+        case '\r':
+            // TODO
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -518,7 +523,13 @@ void editorProcessKeypress(){
             if(E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
             break;
-
+        
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            // TODO
+            break;
+        
         case PAGE_UP:
         case PAGE_DOWN:
             {
@@ -540,6 +551,10 @@ void editorProcessKeypress(){
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+
+        case CTRL_KEY('l'):
+        case '\x1b':
             break;
         
         default:
