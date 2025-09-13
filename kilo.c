b9650aea4d8f6e83156f23ec246906a20f926911
spectrum128 <declandoherty@hotmail.co.uk>
2025-09-13 21:24:33 +0100

    step 164: determine file type and set syntax highlighting.

--- kilo.c
+++ kilo.c
@@ -45,7 +45,15 @@ enum editorHighlight {
     HL_MATCH
 };
 
+#define HL_HIGHLIGHT_NUMBERS (1<<0)
+
 /*** data ***/
+
+struct editorSyntax{
+    char *filetype;
+    char **filematch;
+    int flags;
+};
 
 typedef struct erow {
     int size;
@@ -70,11 +78,26 @@ struct editorConfig{
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
+    struct editorSyntax *syntax;
     // global variable for storing terminal attributes
     struct termios orig_termios;
 };
 
 struct editorConfig E;
+
+/*** filetypes ***/
+
+char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+
+struct editorSyntax HLDB[] = {
+    {
+        "c",
+        C_HL_extensions,
+        HL_HIGHLIGHT_NUMBERS
+    },
+};
+
+#define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 /*** prototypes ***/
 
@@ -242,15 +265,35 @@ int getWindowSize(int *rows, int *cols){
 
 /*** syntax highlighting **/
 
+int is_separator(int c){
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for(i = 0; i < row->rsize; i++){
-        if(isdigit(row->render[i])){
-            row->hl[i] = HL_NUMBER;
+    if(E.syntax == NULL) return;
+
+    int prev_sep = 1;
+
+    int i = 0;
+    while(i < row->size){
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if(E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
+            if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+               (c == '.' && prev_hl == HL_NUMBER)){
+                row->hl[i] = HL_NUMBER;
+                i++;
+                prev_sep = 0;
+                continue;
+            }
         }
+
+        prev_sep = is_separator(c);
+        i++;
     }
 }
 
@@ -259,6 +302,34 @@ int editorSyntaxToColor(int hl){
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
         default: return 37;
+    }
+}
+
+
+void editorSelectSyntaxHighlight(){
+    E.syntax = NULL;
+    if(E.filename == NULL) return;
+
+    char *ext = strrchr(E.filename, '.');
+
+    for(unsigned int j = 0; j < HLDB_ENTRIES; j++){
+        struct editorSyntax *s = &HLDB[j];
+        unsigned int i = 0;
+        while(s->filematch[i]){
+            int is_ext = (s->filematch[i][0] == '.');
+            if((is_ext && ext && !strcmp(ext, s->filematch[i])) || 
+                (!is_ext && strstr(E.filename, s->filematch[i]))){
+              E.syntax = s;
+
+              int filerow;
+              for(filerow = 0; filerow < E.numrows; filerow++){
+                editorUpdateSyntax(&E.row[filerow]);
+              }
+
+              return;
+            }
+            i++;
+        }
     }
 }
 
@@ -450,6 +521,8 @@ void editorOpen(char *filename){
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if(!fp) die("fopen");
 
@@ -473,6 +546,8 @@ void editorSave(){
             editorSetStatusMessage("Save aborted");
             return;
         }
+
+        editorSelectSyntaxHighlight();
     }
 
     int len;
@@ -682,7 +757,8 @@ void editorDrawStatusBar(struct abuf *ab){
         E.filename ? E.filename : "[No Name]", 
         E.numrows, 
         E.dirty ? "(modified)" : "");
-    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d", 
+      E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
     if(len > E.screencols) len = E.screencols;
     abAppend(ab, status, len);
     while(len < E.screencols){
@@ -914,6 +990,7 @@ void initEditor(){
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
+    E.syntax = NULL;
 
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
     E.screenrows -= 2;
