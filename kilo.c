b6b5637f3e1001857e9589e0286f19e2c10f0961
spectrum128 <declandoherty@hotmail.co.uk>
2025-09-13 21:32:37 +0100

    step 167: highlight strings

--- kilo.c
+++ kilo.c
@@ -41,11 +41,13 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /*** data ***/
 
@@ -93,7 +95,7 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
-        HL_HIGHLIGHT_NUMBERS
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 
@@ -276,11 +278,30 @@ void editorUpdateSyntax(erow *row) {
     if(E.syntax == NULL) return;
 
     int prev_sep = 1;
+    int in_string = 0;
 
     int i = 0;
     while(i < row->size){
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if(E.syntax->flags & HL_HIGHLIGHT_STRINGS){
+            if(in_string){
+                row->hl[i] = HL_STRING;
+                if(c == in_string) in_string = 0;
+                i++;
+                prev_sep = 1;
+                continue;
+            } else {
+                if(c == '"' || c == '\''){
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
+                }
+            }
+        }
+
 
         if(E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
             if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -299,6 +320,7 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl){
     switch(hl){
+        case HL_STRING: return 35;
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
         default: return 37;
